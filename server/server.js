47e7f73ddf27b154a079f9c7d803ae75e3b885fa
rulopper <joseraul.muve@gmail.com>
2018-11-21 23:47:18 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
+
+
+
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
